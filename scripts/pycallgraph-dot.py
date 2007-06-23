97d70cf55bf8dbb5b4b01223fbd188cfe3af3efe
gak <gak@c9396a4a-0429-0410-9196-a9af44677a5c>
2007-06-23 23:58:18 +0000

    More work on pycallgraph-dot command line script.

--- scripts/pycallgraph-dot.py
+++ scripts/pycallgraph-dot.py
@@ -27,17 +27,51 @@ from optparse import OptionParser
 import pycallgraph
 
 parser = OptionParser(usage='%prog [options] pythonfile imagefile')
-parser.add_option('-f', '--image-format', dest='format', default='png',
-    help='The image format of imagefile. Default: png')
-parser.add_option('-t', '--tool', dest='tool', default='dot',
-    help='The tool from graphviz to use. Default: dot')
+
+parser.add_option(
+    '-f', '--image-format', dest='format', default='png',
+    help='The image format of imagefile. Default: png',
+    )
+
+parser.add_option(
+    '-t', '--tool', dest='tool', default='dot',
+    help='The tool from graphviz to use. Default: dot',
+    )
+
+parser.add_option(
+    '-i', '--include', dest='include', default=[],
+    action='append',
+    help='Wildcard pattern of modules to include in the output.',
+    )
+
+parser.add_option(
+    '-e', '--exclude', dest='exclude', default=[],
+    action='append',
+    help='Wildcard pattern of modules to exclude in the output. ' \
+        'You can have multiple exclude arguments.'
+    )
+
+parser.add_option(
+    '-d', '--max-depth', dest='max_depth', default=None,
+    help='Maximum stack depth to trace.',
+    )
+
 (options, args) = parser.parse_args()
 
 if len(args) < 2:
     parser.print_help()
     sys.exit(0)
 
-pycallgraph.start_trace()
+# Create globbing filter
+if not options.include:
+    options.include = ['*']
+glob_filter = pycallgraph.GlobbingFilter(
+    include=options.include,
+    exclude=options.exclude,
+    max_depth=options.max_depth,
+    )
+
+pycallgraph.start_trace(filter_func=glob_filter)
 execfile(args[0], globals(), locals())
 pycallgraph.make_dot_graph(args[1], options.format, options.tool)
 
