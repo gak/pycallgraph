32ea4ef78bf664970080255b90f504dffdd905bc
gak <gak@c9396a4a-0429-0410-9196-a9af44677a5c>
2007-06-24 05:38:32 +0000

    docs now include examples

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -1,3 +1,2 @@
-
-epydoc --html -v --graph=classtree --no-frames --url=http://pycallgraph.slowchop.com/ --name=pycallgraph -o/home/httpd/html/slowchop.com/pycallgraph/doc/ /usr/lib/python2.5/site-packages/pycallgraph.py
+epydoc --html -v --graph=classtree --no-frames --url=http://pycallgraph.slowchop.com/ --name=pycallgraph -o/home/httpd/html/slowchop.com/pycallgraph/doc/ pycallgraph.py examples/*py
 
