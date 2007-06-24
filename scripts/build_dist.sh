1e43c7f67d77b862b828e6c37d535758e6fbded2
gak <gak@c9396a4a-0429-0410-9196-a9af44677a5c>
2007-06-24 12:17:03 +0000

    build_dist.sh additions

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -1,4 +1,9 @@
 rm -fr build dist
+python setup.py clean
 python setup.py sdist --formats=gztar,zip
 python setup.py bdist_wininst
+cd dist
+scp * gakman.com:/home/httpd/html/slowchop.com/pycallgraph/download/
+ssh gakman.com chgrp httpd /home/httpd/html/slowchop.com/pycallgraph/download/*
+ssh gakman.com chmod a-wx,a+r /home/httpd/html/slowchop.com/pycallgraph/download/*
 
