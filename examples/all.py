d8e80a2f773e4374dee190abae1f333dcafeca2c
gak <gak@c9396a4a-0429-0410-9196-a9af44677a5c>
2008-07-07 09:18:15 +0000

    branched trunk to releases/0.5.0

--- examples/all.py
+++ examples/all.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from glob import glob
 
 import pycallgraph
--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd ..
 rm -fr build dist
 python setup.py clean
 python setup.py sdist --formats=gztar,zip
 python setup.py bdist_wininst
 cd dist
-scp * gakman.com:/var/html/slowchop.com/pycallgraph/download/
+scp * gakman.com:/var/www/slowchop.com/pycallgraph/download/
 
