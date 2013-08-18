d699958c05541b777f459f180544d9cef29a378d
Gerald Kaszuba <gak@gakman.com>
2013-08-18 16:32:11 +1000

    I can't win can I. Switching to float() instead of __future__.

--- pycallgraph/util.py
+++ pycallgraph/util.py
@@ -1,8 +1,7 @@
-from __future__ import division
-
 class Util(object):
     @staticmethod
     def human_readable_bibyte(num):
+        num = float(num)
         for x in ['B', 'KiB', 'MiB', 'GiB']:
             if num < 1024 and num > -1024:
                 return '{:3.1f}{}'.format(num, x)
