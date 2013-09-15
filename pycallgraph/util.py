9b1710277ecbe6601393835082e47eb2477ecdfc
Gerald Kaszuba <gak@gakman.com>
2013-09-15 13:55:51 +1000

    Oops, left some debugging in there.

--- pycallgraph/util.py
+++ pycallgraph/util.py
@@ -3,7 +3,6 @@ class Util(object):
     @staticmethod
     def human_readable_bibyte(num):
         num = float(num)
-        print(123)
         for x in ['B', 'KiB', 'MiB', 'GiB']:
             if num < 1024 and num > -1024:
                 return '{:3.1f}{}'.format(num, x)
