f751b95437cf8251ca46a97ad6f65ca70c82c3cb
Fabio Caritas Barrionuevo da Luz <bnafta@gmail.com>
2015-03-05 10:57:34 -0300

    pep8

--- test/test_pycallgraph.py
+++ test/test_pycallgraph.py
@@ -1,8 +1,9 @@
 from helpers import *
-from pycallgraph.output import GraphvizOutput
 from pycallgraph import pycall_profile
 
+
 def test_start_no_outputs(pycg):
+
     with pytest.raises(PyCallGraphException):
         pycg.start()
 
@@ -19,6 +20,7 @@ def test_get_tracer_class(pycg):
 
     pycg.config.threaded = False
     assert pycg.get_tracer_class() == SyncronousTracer
+
 
 @pycall_profile()
 def print_something():
